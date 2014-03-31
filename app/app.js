e0f62d64186d2a226fa30163b36984bfce859b0d
Marcell Spies <marcells@gmx.de>
2014-04-01 01:51:43 +0200

    Integrated socket io.

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var socketio = require('socket.io');
 
 var app = express();
 
@@ -31,6 +32,17 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+var io = socketio.listen(server);
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });

0fb6845cd78d09f10fddbfe84800940139ed1d21
Marcell Spies <marcells@gmx.de>
2014-04-03 00:52:53 +0200

    Integrated knockoutjs.

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -1,6 +1,10 @@
-function isNullOrWhiteSpace(str){
-            return str === null || str.match(/^ *$/) !== null;
-        }
+function isNullOrWhiteSpace(str) {
+    if(!str) {
+        return true;
+    }
+
+    return str === null || str.match(/^ *$/) !== null;
+}
 
 function timeOutput(startedAt, finishedAt, lastChangeAt, buildFinished) {
     var output = startedAt.toLocaleString();
@@ -14,54 +18,52 @@ function timeOutput(startedAt, finishedAt, lastChangeAt, buildFinished) {
     return output;
 }
 
-var socket = io.connect('http://localhost:3000');
-socket.on('buildstate', function (data) {
-    console.log(data);
+var BuildViewModel = function (build) {
+    this.buildFinished = ko.observable(build.buildFinished);
+    this.project = ko.observable(build.project);
+    this.definition = ko.observable(build.definition);
+    this.number = ko.observable(build.number);
+    this.startedAt = ko.observable(new Date(build.startedAt));
+    this.finishedAt = ko.observable(new Date(build.finishedAt));
+    this.lastChangeAt = ko.observable(new Date(build.lastChangeAt));
+    this.status = ko.observable(build.status);
+    this.reason = ko.observable(build.reason);
+    this.requestedFor = ko.observable(build.requestedFor);
+    this.hasWarnings = ko.observable(!isNullOrWhiteSpace(build.warnings));
+    this.hasErrors = ko.observable(!isNullOrWhiteSpace(build.errors));
 
-    if (data) {
-        var ul = document.querySelector('#builds');
+    this.cssClass = ko.computed(function () {
+        return this.status() + '-color';
+    }, this);
 
-        ul.innerHTML = '';
+    this.duration = ko.computed(function () {
+        return timeOutput(this.startedAt(), this.finishedAt(), this.lastChangeAt(), this.buildFinished());
+    }, this);
+};
 
-        for(var i=0; i < data.builds.length; i++) {
-            var build = data.builds[i];
-            var startedAt = new Date(build.startedAt);
-            var finishedAt = new Date(build.finishedAt);
-            var lastChangeAt = new Date(build.lastChangeAt);
+var AppViewModel = function() {
+    this.builds = ko.observableArray([]);
+};
 
-            var li = '<div class="flex-item {COLOR}">'
-                + '<span class="title">'
-                + build.project
-                + '</span>'
-                + ' -> '
-                + '<span class="title">'
-                + build.definition
-                + '</span>'
-                + ' -> '
-                + '<span class="title">'
-                + build.number
-                + '</span>'
-                + '<div class="duration">'
-                + timeOutput(startedAt, finishedAt, lastChangeAt, build.buildFinished)
-                + '<div>'
-                + '<div class="status">'
-                + build.status
-                + '</div>'
-                + '<span class="reason">'
-                + build.reason
-                + '</span>'
-                + ' for '
-                + '<span class="for">'
-                + build.requestedFor
-                + '</span>'
-                + '<div class="warnings">'
-                + '[ warnings: ' + !isNullOrWhiteSpace(build.warnings) + ', errors: ' + !isNullOrWhiteSpace(build.errors) + ']'
-                + '</div>'
-                + '</div>';
+var app = new AppViewModel();
 
-            li = li.replace('{COLOR}', build.status + '-color');
+$(function() {
+    app.builds.push(new BuildViewModel({ project: 'Loading...'}));
+    ko.applyBindings(app);
 
-            ul.innerHTML += li;
+    var socket = io.connect('http://keller:3000');
+
+    socket.on('buildstate', function (data) {
+        if (data) {
+            console.log(data);
+
+            app.builds.removeAll();
+
+            for(var i=0; i < data.builds.length; i++) {
+                var build = data.builds[i];
+                
+                app.builds.push(new BuildViewModel(build));
+            }
         }
-    }
+    });
 });

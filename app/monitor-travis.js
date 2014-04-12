d3417b178b7db90e50d090fd0e3f4e04870a7784
Marcell Spies <marcells@gmx.de>
2014-04-12 19:06:17 +0200

    Added author_name of travis builds.

--- app/monitor-travis.js
+++ app/monitor-travis.js
@@ -1,4 +1,5 @@
-var request = require('request');
+var request = require('request'),
+    async = require('async');
 
 module.exports = function () {
     var self = this,
@@ -9,6 +10,22 @@ module.exports = function () {
                 },
                 function(error, response, body) {
                     callback(body);
+            });
+        },
+        queryBuilds = function (callback) {
+            requestBuilds(function (body) {
+                async.map(body, requestBuild, function (err, results) {
+                    callback(results);
+                });
+            });
+        },
+        requestBuild = function (build, callback) {
+            request({ 
+                'url': 'https://api.travis-ci.org/repos/' + self.configuration.slug + '/builds/' + build.id,
+                'json' : true
+                },
+                function(error, response, body) {
+                    callback(error, simplifyBuild(body));
             });
         },
         parseDate = function (dateAsString) {
@@ -26,23 +43,17 @@ module.exports = function () {
             return {
                 id: self.configuration.slug + '|' + res.number,
                 project: self.configuration.slug,
-                definition: 'TODO',
                 number: res.number,
                 isRunning: res.state === 'started',
                 startedAt: parseDate(res.started_at),
                 finishedAt: parseDate(res.finished_at),
-                requestedFor: 'TODO',
+                requestedFor: res.author_name,
                 status: getStatus(res.result, res.state),
                 statusText: res.state,
                 reason: res.event_type,
                 hasErrors: false,
                 hasWarnings: false
             };
-        },
-        queryBuilds = function (callback) {
-            requestBuilds(function (body) {
-                callback(body.map(simplifyBuild));
-            });
         };
 
     self.configure = function (config) {

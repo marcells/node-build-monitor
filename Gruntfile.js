5ed3c19283f7f25945a8161722c8abebfc464d22
Marcell Spies <marcells@gmx.de>
2014-04-13 12:32:32 +0200

    Added travis ci support.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('ci', ['jshint']);
+  grunt.registerTask('default', ['jshint']);
 };

5b968dac0b9593023d37809de32bcf01e662611e
Marcell Spies <marcells@gmx.de>
2014-12-16 23:45:05 +0100

    Added docker integration and refactored configuration.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM dockerfile/nodejs
+
+RUN npm install -g forever
+
+WORKDIR /build-mon
+
+ADD app /build-mon/app
+ADD package.json /build-mon/package.json
+ADD README.md /build-mon/README.md
+
+RUN npm install
+
+ONBUILD ADD config.json /build-mon/app/config.json
+
+EXPOSE 3000
+
+CMD [ "forever", "/build-mon/app/app.js" ]
--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
+var config = require('./config');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -17,7 +17,6 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
@@ -34,7 +33,7 @@ var io = socketio.listen(server);
 io.set('log level', 1);
 
 server.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('node-build-monitor is listening on port ' + app.get('port'));
 });
 
 io.sockets.on('connection', function (socket) {
@@ -42,65 +41,18 @@ io.sockets.on('connection', function (socket) {
 });
 
 var Monitor = require('./monitor'),
-    Tfs = require('./monitors/Tfs'),
-    Travis = require('./monitors/Travis'),
-    monitor = new Monitor(),
-    tfs,
-    tfs2,
-    travis,
-    travis2;
+    monitor = new Monitor();
 
-if(process.env.TFS1_ACTIVE) {
-    tfs = new Tfs(),
+for (var i = 0; i < config.services.length; i++) {
+    var serviceConfig = config.services[i];
+    var service = new (require('./monitors/' + serviceConfig.name))();
+    
+    service.configure(serviceConfig.configuration);
 
-    tfs.configure({
-        server: process.env.TFS1_ODATA_WRAPPER, // 'https://odatawrapper'
-        user: process.env.TFS1_USER, // 'Domain\User'
-        password: process.env.TFS1_PASSWORD, // 'Password'
-        collection: process.env.TFS1_COLLECTION || 'DefaultCollection'
-    });
+    monitor.watchOn(service);
+};
 
-    monitor.watchOn(tfs);
-}
-
-if(process.env.TFS2_ACTIVE) {
-    tfs2 = new Tfs(),
-
-    tfs2.configure({
-        server: process.env.TFS2_ODATA_WRAPPER, // 'https://odatawrapper'
-        user: process.env.TFS2_USER, // 'Domain\User'
-        password: process.env.TFS2_PASSWORD, // 'Password'
-        collection: process.env.TFS2_COLLECTION || 'DefaultCollection'
-    });
-
-    monitor.watchOn(tfs2);
-}
-
-if(process.env.TRAVIS1_ACTIVE) {
-    travis = new Travis();
-
-    travis.configure({
-        slug: process.env.TRAVIS1_SLUG
-    });
-
-    monitor.watchOn(travis);
-}
-
-if(process.env.TRAVIS2_ACTIVE) {
-    travis2 = new Travis();
-
-    travis2.configure({
-        slug: process.env.TRAVIS2_SLUG
-    });
-
-    monitor.watchOn(travis2);
-}
-
-monitor.configure({
-    interval: 5000,
-    numberOfBuilds: 12,
-    debug: true
-});
+monitor.configure(config.monitor);
 
 monitor.on('buildsLoaded', function (builds) {
   io.sockets.emit('buildsLoaded', monitor.currentBuilds);

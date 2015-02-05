8735c5dbd3e4009f617843503d45e6669fc1e570
Marcell Spies <marcells@gmx.de>
2015-02-06 00:54:51 +0100

    Switched travis service to es6.

--- app/services/Travis.js
+++ app/services/Travis.js
@@ -1,72 +1,68 @@
-var request = require('request'),
-    async = require('async');
+import request from 'request';
+import async from 'async';
 
-module.exports = function () {
-    var self = this,
-        requestBuilds = function (callback) {
-            request({ 
-                'url': 'https://api.travis-ci.org/repos/' + self.configuration.slug + '/builds',
-                'json' : true
-                },
-                function(error, response, body) {
-                    callback(body);
-            });
-        },
-        queryBuilds = function (callback) {
-            requestBuilds(function (body) {
-                if (!body) {
-                    callback([]);
-                } else {
-                    async.map(body, requestBuild, function (err, results) {
-                        callback(results);
-                    });
-                }
-            });
-        },
-        requestBuild = function (build, callback) {
-            request({ 
-                'url': 'https://api.travis-ci.org/repos/' + self.configuration.slug + '/builds/' + build.id,
-                'json' : true
-                },
-                function(error, response, body) {
-                    callback(error, simplifyBuild(body));
-            });
-        },
-        parseDate = function (dateAsString) {
-            return new Date(dateAsString);
-        },
-        getStatus = function (result, state) {
-            if (state === 'started') return "Blue";
-            if (state === 'created') return "Blue";
-            if (state === 'canceled') return "Gray";
-            if (result === null || result === 1) return "Red";
-            if (result === 0) return "Green";
-            
-            return null;
-        },
-        simplifyBuild = function (res) {
-            return {
-                id: self.configuration.slug + '|' + res.number,
-                project: self.configuration.slug,
-                number: res.number,
-                isRunning: res.state === 'started',
-                startedAt: parseDate(res.started_at),
-                finishedAt: parseDate(res.finished_at),
-                requestedFor: res.author_name,
-                status: getStatus(res.result, res.state),
-                statusText: res.state,
-                reason: res.event_type,
-                hasErrors: false,
-                hasWarnings: false,
-                url: 'https://travis-ci.org/' + self.configuration.slug + '/builds/' + res.id
-            };
-        };
+export default function() {
+  let requestBuilds = callback => {
+      request({
+        'url': 'https://api.travis-ci.org/repos/' + this.configuration.slug + '/builds',
+        'json': true
+      }, (error, response, body) => callback(body));
+    },
 
-    self.configure = function (config) {
-        self.configuration = config;
+    queryBuilds = callback => {
+      requestBuilds(function(body) {
+        if (!body) {
+          callback([]);
+        } else {
+          async.map(body, requestBuild, function(err, results) {
+            callback(results);
+          });
+        }
+      });
+    },
+
+    requestBuild = (build, callback) => {
+      request({
+        'url': 'https://api.travis-ci.org/repos/' + this.configuration.slug + '/builds/' + build.id,
+        'json': true
+      }, (error, response, body) => callback(error, simplifyBuild(body)));
+    },
+
+    parseDate = dateAsString => new Date(dateAsString),
+
+    getStatus = (result, state) => {
+      if (state === 'started') return "Blue";
+      if (state === 'created') return "Blue";
+      if (state === 'canceled') return "Gray";
+      if (result === null || result === 1) return "Red";
+      if (result === 0) return "Green";
+
+      return null;
+    },
+
+    simplifyBuild = res => {
+      return {
+        id: this.configuration.slug + '|' + res.number,
+        project: this.configuration.slug,
+        number: res.number,
+        isRunning: res.state === 'started',
+        startedAt: parseDate(res.started_at),
+        finishedAt: parseDate(res.finished_at),
+        requestedFor: res.author_name,
+        status: getStatus(res.result, res.state),
+        statusText: res.state,
+        reason: res.event_type,
+        hasErrors: false,
+        hasWarnings: false,
+        url: 'https://travis-ci.org/' + this.configuration.slug + '/builds/' + res.id
+      };
     };
 
-    self.check = function (callback) {
-        queryBuilds(callback);
-    };
+  this.configure = function(config) {
+    this.configuration = config;
+  };
+
+  this.check = function(callback) {
+    queryBuilds(callback);
+  };
 };

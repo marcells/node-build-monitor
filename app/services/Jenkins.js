7d3b0808f88790cf81b8dc2feb29366da355bca8
Marcell Spies <marcells@gmx.de>
2015-02-11 23:19:00 +0100

    Changed Jenkins service to ES6.

--- app/services/Jenkins.js
+++ app/services/Jenkins.js
@@ -1,115 +1,109 @@
-var jenkinsapi = require('jenkins-api'),
-    async = require('async'),
-    makeRequestConfiguration = function (serviceConfiguration) {
-        var tryApplyAuthHeader = function (requestConfig, key, value) {
-            if (!value) return;
+import jenkinsapi from 'jenkins-api';
+import async from 'async';
 
-            requestConfig.auth = requestConfig.auth || {};
-            requestConfig.auth[key] = value;
-        };
+let makeRequestConfiguration = serviceConfiguration => {
+  let tryApplyAuthHeader = (requestConfig, key, value) => {
+    if (!value) return;
 
-        var requestConfig = serviceConfiguration.options || {};
+    requestConfig.auth = requestConfig.auth || {};
+    requestConfig.auth[key] = value;
+  };
 
-        tryApplyAuthHeader(requestConfig, 'user', serviceConfiguration.username);
-        tryApplyAuthHeader(requestConfig, 'pass', serviceConfiguration.password);
+  let requestConfig = serviceConfiguration.options || {};
 
-        return requestConfig;
-    };
+  tryApplyAuthHeader(requestConfig, 'user', serviceConfiguration.username);
+  tryApplyAuthHeader(requestConfig, 'pass', serviceConfiguration.password);
 
-module.exports = function () {
-    var self = this,
-        jenkins,
-        requestBuilds = function (callback) {
-            jenkins.job_info(self.configuration.job, function(error, data) {
-                callback(data.builds);
-            });
-        },
-        requestBuild = function (build, callback) {
-            jenkins.build_info(self.configuration.job, build.number, function(error, data) {
-                callback(error, simplifyBuild(data));
-            });
-        },
-        queryBuilds = function (callback) {
-            requestBuilds(function (body) {
-                async.map(body, requestBuild, function (error, results) {
-                    callback(results);
-                });
-            });
-        },
-        parseDate = function (dateAsString) {
-            return new Date(dateAsString);
-        },
-        getStatus = function (result) {
-            if (result === 'FAILURE') return "Red";
-            if (result === 'SUCCESS') return "Green";
-            if (result === 'UNSTABLE') return "Green";
-            if (result === 'NOT_BUILT') return "Blue";
-            if (result === 'ABORTED') return "Gray";
-            if (result === null) return "Blue";
-
-            return null;
-        },
-        getStatusText = function (result) {
-            if (result === 'FAILURE') return "Failure";
-            if (result === 'SUCCESS') return "Success";
-            if (result === 'UNSTABLE') return "Unstable";
-            if (result === 'NOT_BUILT') return "Running";
-            if (result === 'ABORTED') return "Aborted";
-            if (result === null) return "Running";
-
-            return null;
-        },
-        getRequestedFor = function (build) {
-            if(build.actions) {
-                for (var i = 0; i < build.actions.length; i++) {
-                    var action = build.actions[i];
-                    if ((action) && (action.causes)) {
-                        for (var j = 0; j < action.causes.length; j++) {
-                            var cause = action.causes[j];
-                            if(cause && cause.userName) {
-                                return cause.userName;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return "Unknown";
-        },
-        simplifyBuild = function (res) {
-            return {
-                id: self.configuration.job + '|' + res.id,
-                project: self.configuration.job,
-                number: res.number,
-                isRunning: res.building,
-                startedAt: parseDate(res.timestamp),
-                finishedAt: parseDate(res.timestamp + res.duration),
-                requestedFor: getRequestedFor(res),
-                status: getStatus(res.result),
-                statusText: getStatusText(res.result),
-                reason: "Build",
-                hasErrors: false,
-                hasWarnings: res.result == 'UNSTABLE',
-                url: self.configuration.url + '/job/' + self.configuration.job + '/' + res.number
-            };
-        };
-
-    self.configure = function (config) {
-        self.configuration = config;
-
-        if (self.configuration.url.indexOf('@') > -1) {
-            throw new Error(
-                'Breaking Configuration change:\n' +
-                'To display build details, the url parameter is now published to the client. \n' +
-                'This leads to a security risk, cause your credentials would also be published. \n' +
-                'Please use now the \'username\' and \'password\' options. \n' +
-                'More information on: https://github.com/marcells/node-build-monitor#jenkins \n\n');
-        }
-
-        jenkins = jenkinsapi.init(self.configuration.url, makeRequestConfiguration(self.configuration));
-    };
-
-    self.check = function (callback) {
-        queryBuilds(callback);
-    };
+  return requestConfig;
 };
+
+export default function() {
+  let jenkins,
+    requestBuilds = callback => {
+      jenkins.job_info(this.configuration.job, (error, data) => {
+        callback(data.builds);
+      });
+    },
+    requestBuild = (build, callback) => {
+      jenkins.build_info(this.configuration.job, build.number, (error, data) => {
+        callback(error, simplifyBuild(data));
+      });
+    },
+    queryBuilds = callback => {
+      requestBuilds(body => {
+        async.map(body, requestBuild, (error, results) => callback(results));
+      });
+    },
+    parseDate = dateAsString => new Date(dateAsString),
+    getStatus = result => {
+      if (result === 'FAILURE') return "Red";
+      if (result === 'SUCCESS') return "Green";
+      if (result === 'UNSTABLE') return "Green";
+      if (result === 'NOT_BUILT') return "Blue";
+      if (result === 'ABORTED') return "Gray";
+      if (result === null) return "Blue";
+
+      return null;
+    },
+    getStatusText = result => {
+      if (result === 'FAILURE') return "Failure";
+      if (result === 'SUCCESS') return "Success";
+      if (result === 'UNSTABLE') return "Unstable";
+      if (result === 'NOT_BUILT') return "Running";
+      if (result === 'ABORTED') return "Aborted";
+      if (result === null) return "Running";
+
+      return null;
+    },
+    getRequestedFor = build => {
+      if (build.actions) {
+        for (let action of build.actions) {
+          if (action && action.causes) {
+            for (let cause of action.causes) {
+              if (cause && cause.userName) {
+                return cause.userName;
+              }
+            }
+          }
+        }
+      }
+
+      return "Unknown";
+    },
+    simplifyBuild = res => {
+      return {
+        id: this.configuration.job + '|' + res.id,
+        project: this.configuration.job,
+        number: res.number,
+        isRunning: res.building,
+        startedAt: parseDate(res.timestamp),
+        finishedAt: parseDate(res.timestamp + res.duration),
+        requestedFor: getRequestedFor(res),
+        status: getStatus(res.result),
+        statusText: getStatusText(res.result),
+        reason: "Build",
+        hasErrors: false,
+        hasWarnings: res.result == 'UNSTABLE',
+        url: this.configuration.url + '/job/' + this.configuration.job + '/' + res.number
+      };
+    };
+
+  this.configure = config => {
+    this.configuration = config;
+
+    if (this.configuration.url.indexOf('@') > -1) {
+      throw new Error(
+        'Breaking Configuration change:\n' +
+        'To display build details, the url parameter is now published to the client. \n' +
+        'This leads to a security risk, cause your credentials would also be published. \n' +
+        'Please use now the \'username\' and \'password\' options. \n' +
+        'More information on: https://github.com/marcells/node-build-monitor#jenkins \n\n');
+    }
+
+    jenkins = jenkinsapi.init(this.configuration.url, makeRequestConfiguration(this.configuration));
+  };
+
+  this.check = callback => {
+    queryBuilds(callback);
+  };
+}

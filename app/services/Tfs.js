bf464d451172287144b14aff6efb24abde38fc24
Marcell Spies <marcells@gmx.de>
2015-02-12 00:01:37 +0100

    Changed Tfs service to ES6.

--- app/services/Tfs.js
+++ app/services/Tfs.js
@@ -1,85 +1,78 @@
-var request = require('request');
+import request from 'request';
 
-module.exports = function () {
-    var self = this,
-        makeUrl = function (url, odata) {
-            var baseUrl = 'https://tfsodata.visualstudio.com/' + self.configuration.collection + url;
+export default function() {
+  let makeUrl = (url, odata) => {
+      let baseUrl = 'https://tfsodata.visualstudio.com/' + this.configuration.collection + url;
 
-            if (odata) {
-                baseUrl += '?' + odata;
-            }
+      if (odata) {
+        baseUrl += '?' + odata;
+      }
 
-            return baseUrl;
+      return baseUrl;
+    },
+    makeRequest = (url, callback) => {
+      request({
+        'url': url,
+        'rejectUnauthorized': false,
+        'headers': {
+          'Accept': 'application/json'
         },
-        makeRequest = function (url, callback) {
-            request({ 
-                'url': url,
-                'rejectUnauthorized': false,
-                'headers': { 'Accept': 'application/json' },
-                'json' : true,
-                'auth': { 'user': self.configuration.accountname + '\\' + self.configuration.username, 'pass': self.configuration.password }
-                },
-                function(error, response, body) {
-                    callback(body);
-            });
-        },
-        parseDate = function (dateAsString) {
-            return new Date(parseInt(dateAsString.substr(6)));
-        },
-        forEachResult = function (body, callback) {
-            for (var i = 0; i < body.d.results.length; i++) {
-                callback(body.d.results[i]);
-            }
-        },
-        isNullOrWhiteSpace = function (string) {
-            if(!string) {
-                return true;
-            }
+        'json': true,
+        'auth': {
+          'user': this.configuration.accountname + '\\' + this.configuration.username,
+          'pass': this.configuration.password
+        }
+      }, (error, response, body) => callback(body));
+    },
+    parseDate = dateAsString => new Date(parseInt(dateAsString.substr(6))),
+    forEachResult = (body, callback) => {
+      for (let result of body.d.results) {
+        callback(result);
+      }
+    },
+    isNullOrWhiteSpace = string => !string || string === null || string.match(/^ *$/) !== null,
+    getStatus = statusText => {
+      if (statusText === "Succeeded") return "Green";
+      if (statusText === "Failed") return "Red";
+      if (statusText === "InProgress") return "Blue";
+      if (statusText === "Stopped") return "Gray";
+      if (statusText === "PartiallySucceeded") return "'#FFA500'";
 
-            return string === null || string.match(/^ *$/) !== null;
-        },
-        getStatus = function (statusText) {
-            if (statusText === "Succeeded") return "Green";
-            if (statusText === "Failed") return "Red";
-            if (statusText === "InProgress") return "Blue";
-            if (statusText === "Stopped") return "Gray";
-            if (statusText === "PartiallySucceeded") return "'#FFA500'";
+      return null;
+    },
+    simplifyBuild = res => {
+      return {
+        id: res.Project + '|' + res.Definition + '|' + res.Number,
+        project: res.Project,
+        definition: res.Definition,
+        number: res.Number,
+        isRunning: !res.BuildFinished,
+        startedAt: parseDate(res.StartTime),
+        finishedAt: parseDate(res.FinishTime),
+        requestedFor: res.RequestedFor,
+        statusText: res.Status,
+        status: getStatus(res.Status),
+        reason: res.Reason,
+        hasErrors: !isNullOrWhiteSpace(res.Errors),
+        hasWarnings: !isNullOrWhiteSpace(res.Warnings)
+      };
+    },
+    queryBuilds = callback => {
+      let builds = [];
+      makeRequest(makeUrl('/Builds', '$top=12&$orderby=StartTime%20desc'), body => {
+        forEachResult(body, res => {
+          builds.push(simplifyBuild(res));
+        });
 
-            return null;
-        },
-        simplifyBuild = function (res) {
-            return {
-                id: res.Project + '|' + res.Definition + '|' + res.Number,
-                project: res.Project,
-                definition: res.Definition,
-                number: res.Number,
-                isRunning: !res.BuildFinished,
-                startedAt: parseDate(res.StartTime),
-                finishedAt: parseDate(res.FinishTime),
-                requestedFor: res.RequestedFor,
-                statusText: res.Status,
-                status: getStatus(res.Status),
-                reason: res.Reason,
-                hasErrors: !isNullOrWhiteSpace(res.Errors),
-                hasWarnings: !isNullOrWhiteSpace(res.Warnings)
-            };
-        },
-        queryBuilds = function (callback) {
-            var builds = [];
-            makeRequest(makeUrl('/Builds', '$top=12&$orderby=StartTime%20desc'), function (body) {
-                forEachResult(body, function (res) {
-                    builds.push(simplifyBuild(res));
-                });
-
-                callback(builds);
-            });
-        };
-
-    self.configure = function (config) {
-        self.configuration = config;
+        callback(builds);
+      });
     };
 
-    self.check = function (callback) {
-        queryBuilds(callback);
-    };
-};
+  this.configure = config => {
+    this.configuration = config;
+  };
+
+  this.check = callback => {
+    queryBuilds(callback);
+  };
+}
